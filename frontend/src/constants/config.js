b9bcb5f731f65cbc752410c707ed6aecd0363349
Amber-Kim <[amber.kje@gmail.com]>
2022-09-24 02:47:54 -0400

    Update

--- frontend/src/constants/config.js
+++ frontend/src/constants/config.js
@@ -3,6 +3,6 @@ const queryClient = new QueryClient();
 
 const AXIOS_URL = 
   process.env.NODE_ENV === "production"
-    ? "http://accountbookamber:5000"
+    ? ""
     : "http://localhost:5000/api/"
 export { AXIOS_URL, queryClient };
